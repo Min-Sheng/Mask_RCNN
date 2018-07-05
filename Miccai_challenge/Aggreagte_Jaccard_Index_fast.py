8705fa084dfe063323c461e1ed158548311316fa
Min Sheng, Wu <30727252+Min-Sheng@users.noreply.github.com>
2018-07-05 16:22:58 +0800

    Correct the bug in the fast AJI function.

--- Miccai_challenge/Aggreagte_Jaccard_Index_fast.py
+++ Miccai_challenge/Aggreagte_Jaccard_Index_fast.py
@@ -12,20 +12,15 @@ def agg_jc_index(mask, pred):
     c = 0   # intersection
     u = 0   # unio
     pred_mark_used = []
-    pred_mark_used_flag=np.zeros(len(mask[0,0,:]),dtype=bool)
     for idx_m in tqdm_notebook(range(len(mask[0,0,:]))):
         m = mask[:,:,idx_m]
         intersect_list = []
         union_list = []
         for idx_pred in range(1, int(np.max(pred))+1):
-            if pred_mark_used_flag[idx_pred-1] == False:
-                p = (pred==idx_pred)
-                intersect = np.count_nonzero(m & p)
-                union = np.count_nonzero(m) + np.count_nonzero(p) - intersect
+            p = (pred==idx_pred)
+            intersect = np.count_nonzero(m & p)
+            union = np.count_nonzero(m) + np.count_nonzero(p) - intersect
             #print(intersect, union)
-            else:
-                intersect=0
-                union=1
             intersect_list.append(intersect)
             union_list.append(union)
             
