4871c5ba6727b7a3595af0c1540323dfe1f87783
Min Sheng, Wu <30727252+Min-Sheng@users.noreply.github.com>
2018-07-27 13:02:38 +0800

    Accelerate more.

--- Miccai_challenge/Aggreagte_Jaccard_Index_fast.py
+++ Miccai_challenge/Aggreagte_Jaccard_Index_fast.py
@@ -1,3 +1,25 @@
+import numpy as np
+import tqdm
+from tqdm import tqdm_notebook
+import numexpr as ne
+
+def compute_intersect_union(m, pred, pred_mark_isused, idx_pred):
+    # check the prediction has been used or not
+    if pred_mark_isused[idx_pred]:
+        intersect = 0
+        union = np.count_nonzero(m)
+    else:
+        p = (pred == idx_pred)
+        # replace multiply with bool operation
+        s = ne.evaluate("m&p")
+        intersect = np.count_nonzero(s)
+        #intersect = np.count_nonzero(m & p)
+        #u1 = np.count_nonzero(m)
+        #u2 = np.count_nonzero(p)
+        #union = ne.evaluate("u1+u2-intersect")
+        union = np.count_nonzero(m) + np.count_nonzero(p) - intersect
+    return (intersect, union)
+
 # fast version of Aggregated Jaccrd Index
 def agg_jc_index(mask, pred):
     """Calculate aggregated jaccard index for prediction & GT mask
@@ -8,7 +30,8 @@ def agg_jc_index(mask, pred):
 
     Returns: Aggregated Jaccard index for GT & mask 
     """
-
+    
+    mask=mask.astype(np.bool)
     c = 0 # count intersection
     u = 0 # count union
     tqdm.monitor_interval = 0 # disable tqdm monitor to prevent warning message
@@ -17,28 +40,34 @@ def agg_jc_index(mask, pred):
     pred_mark_isused = np.zeros((pred_instance+1), dtype=bool)
     
     for idx_m in tqdm_notebook(range(len(mask[0,0,:]))):
-        m = mask[:,:,idx_m]
-        intersect_list = []
-        union_list = []
-        iou_list = []
+        # m = mask[:,:,idx_m]
+        m = np.take(mask, idx_m, axis=2)
+        #intersect_list = []
+        #union_list = []
+        #iou_list = []
         
+        intersect_list, union_list = zip(*[compute_intersect_union(m, pred, pred_mark_isused, idx_pred) for idx_pred in range(1, pred_instance+1)])
+        #print(intersect_list)
+        """
         for idx_pred in range(1, pred_instance+1):
             # check the prediction has been used or not
             if pred_mark_isused[idx_pred] == True:
                 intersect = 0
-                union = np.count_nonzero(m!=0)
+                union = np.count_nonzero(m)
             else:
                 p = (pred == idx_pred)
                 
                 # replace multiply with bool operation 
-                intersect = np.count_nonzero((m!=0) & p)
-                union = np.count_nonzero(m!=0) + np.count_nonzero(p) - intersect
+                s = ne.evaluate("m&p")
+                intersect = np.count_nonzero(s)
+                union = np.count_nonzero(m) + np.count_nonzero(p) - intersect
             
             intersect_list.append(intersect)
             union_list.append(union)
-            
+            #print(intersect_list)
+        """
         iou_list = np.array(intersect_list) / np.array(union_list)    
-        hit_idx = np.argmax(np.array(iou_list))
+        hit_idx = np.argmax(iou_list)
         c += intersect_list[hit_idx]
         u += union_list[hit_idx]
         pred_mark_used.append(hit_idx)
